3f6421d6a91eff8531d22b097b8139d9ad830040
Nikita Khilioti <n.khilioti@gmail.com>
2022-02-02 18:43:50 +0200

    config interface

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
+
+function App() {
+const todos = [
+  {id: 1, complited: false, title: 'Купить хлеб'},
+  {id: 2, complited: false, title: 'Купить колбасу'},
+  {id: 3, complited: false, title: 'Купить масло'}
+]
+
+
+  return (
+   <div className="wrapper">
+      <h1>React tutorial</h1>
+
+      <TodoList todos={todos} />
+   </div>
+  );
+}
+
+export default App;
